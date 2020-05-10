58c54dbc29e928d95bddcfc4d927aa855b6259b9
nicoleparisis <nmtierney@mail.usf.edu>
2020-05-10 12:15:12 -0400

    fix orientation checker

--- ios/RN/RNSensorOrientationChecker.h
+++ ios/RN/RNSensorOrientationChecker.h
@@ -1,5 +1,21 @@
-#import <React/RCTBridgeModule.h>
-#import "FaceDetectorManagerMlkit.h"
+//
+//  RNSensorOrientationChecker.h
+//  RNCamera
+//
+//  Created by Radu Popovici on 24/03/16.
+//
+//
 
-@interface RNFaceDetectorModuleMLKit : NSObject <RCTBridgeModule>
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+typedef void (^RNSensorCallback)(UIInterfaceOrientation orientation);
+
+@interface RNSensorOrientationChecker : NSObject
+
+@property(assign, nonatomic) UIInterfaceOrientation orientation;
+
+- (void)getDeviceOrientationWithBlock:(RNSensorCallback)callback;
+- (AVCaptureVideoOrientation)convertToAVCaptureVideoOrientation:(UIInterfaceOrientation)orientation;
+
 @end
